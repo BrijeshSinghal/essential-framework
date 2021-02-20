dd8ca5fa32abd800cf4c177d8429b330b56ba873
Brijesh Singhal <login.brijeshmohansinghal@gmail.com>
2021-02-20 13:29:44 +0530

    separated website

--- website/js/app.js
+++
@@ -1,7 +0,0 @@
-let tags = document.querySelectorAll("*");
-let html = ``;
-tags.forEach((tag) => {
-	if (tag.className) html += `${tag}: ${tag.className}<br>`;
-});
-
-document.querySelector("#source").innerHTML = html;
